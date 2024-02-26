f5ac22fd0fedf2e5f7b66cb026956c7a282b1e94
clarspau <125636195+clarspau@users.noreply.github.com>
2024-02-25 21:15:44 -0800

    Part 1 done

--- part-1/App.js
+++ part-1/App.js
@@ -1,8 +1,11 @@
+/** renders a <div> with instances of the other two components. */
+
 function App() {
      const root = ReactDOM.createRoot(document.getElementById('root'));
      root.render(
           <div>
-
+               <FirstComponent />
+               <NamedComponent name="Hoka" />
           </div>
      );
 }
